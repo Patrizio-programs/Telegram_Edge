b22d24fd268786c205beeb07866def7462c02eb3
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-03-30 09:55:40 -0400

    Add files via upload

--- EdgeGPT/src/app.py
+++ EdgeGPT/src/app.py
@@ -18,6 +18,11 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi, I'm the EdgeGPT chatbot. Send me a message and I'll respond with something interesting!")
+    
+    
+@dp.message_handler(commands=['about'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hey there, I'm Patrick Medley, a lover of all things coding and proud Jamaican 🇯🇲 . I've always had a fascination with technology and the endless possibilities it presents. From building websites to developing software, I've dabbled in it all. I find great joy in creating things that not only look good but work seamlessly. If you have any questions or concerns about the bot, feel free to shoot me an email at pstriziomedley@gmail.com. I'm always happy to connect with fellow coders or anyone interested in the tech industry. Thanks for stopping by!")
 
 @dp.message_handler()
 async def echo_all(message: types.Message):
@@ -42,7 +47,6 @@ async def echo_all(message: types.Message):
     
     text = '\n'.join(response_messages)
         
-    
 
     # Send the response back to the user
     await message.reply(text)
